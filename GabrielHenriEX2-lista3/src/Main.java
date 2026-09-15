dbdda6e9fb785cf636a6200897f4a37dcd1ae990
GabrielOliveira007-ux <oliveirarochagabrielhenrique@gmail.com>
2026-09-15 16:43:52 -0300

    Add files via upload

---
+++ GabrielHenriEX2-lista3/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public static void main(String[] args) {
+    // Laço for para percorrer de 1 até 10
+    for (int i = 10; i >= 1; i--) {
+        System.out.println("Contagem ="+ i);
+    }
+    System.out.println(" DECOLAR");
+}
+
+
---
+++ GabrielHenriEX3-lista3/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public static void main(String[] args) {
+    // Laço for para percorrer de 1 até 10
+    for (int i = 2; i <= 20; i+=2) {
+        System.out.println("Contagem ="+ i);
+    }
+    System.out.println("apenas números pares");
+}
+
+
